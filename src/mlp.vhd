9393344bf6452dbd9bfcf8b68146afe4b2282c7b
Alden Davidson <adavidson@protonmail.ch>
2018-11-18 16:12:01 -0500

    added initial src skeleton

---
+++ src/mlp.vhd
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/18/2018 04:08:57 PM
+-- Design Name: 
+-- Module Name: mlp - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+entity mlp is
+--  Port ( );
+end mlp;
+
+architecture Behavioral of mlp is
+
+begin
+
+
+end Behavioral;
